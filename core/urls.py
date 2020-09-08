7b5565d5cad30744ea7a18c77f6065c892b3e415
Jonas G F Pettersson <jonas.pettersson@siemens.com>
2020-09-08 17:59:17 +0200

    test views

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('detail/<int:question_id>', views.detail, name='detail'),
+    path('results/<int:question_id>', views.results, name='results'),
+    path('vote/<int:question_id>', views.vote, name='vote'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse('Hello!')
+
+
+def detail(request, question_id):
+    return HttpResponse(f'You are looking at question {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'You are looking at the results of question {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'You are looking at the vote of question {question_id}')
