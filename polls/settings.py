8887ada2d5c3ed88e9667a29f701d28e59d0b783
Jonas G F Pettersson <jonas.pettersson@siemens.com>
2020-09-08 16:18:26 +0200

    added postgres

--- polls/settings.py
+++ polls/settings.py
@@ -75,6 +75,12 @@ WSGI_APPLICATION = 'polls.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'polls',
+        'USER': 'postgres',
+        'PASSWORD': 'Sanoj123'
+    },
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
