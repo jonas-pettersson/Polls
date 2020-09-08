9017e7ce0dd135e64d236fdbbcbe538dea0402c1
Jonas G F Pettersson <jonas.pettersson@siemens.com>
2020-09-08 17:27:40 +0200

    polls model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+import datetime
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    date_published = models.DateTimeField()
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.date_published >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
