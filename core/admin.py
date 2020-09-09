754e2e5ebde228734b21db4403237d056df23dcb
Jonas G F Pettersson <jonas.pettersson@siemens.com>
2020-09-09 18:16:47 +0200

    admin page customization

--- core/admin.py
+++ core/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
 admin.site.register(Choice)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['date_published', 'question_text']
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'date_published',
+                    'was_published_recently')
+    list_filter = ['date_published']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
